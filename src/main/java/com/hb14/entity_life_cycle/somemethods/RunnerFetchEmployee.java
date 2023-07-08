af7ebcf279a629b475be547f3716b1767c9973cb
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-07-08 20:11:35 +0300

    Get Load Entity LifeCycle

--- src/main/java/com/hb14/entity_life_cycle/somemethods/RunnerFetchEmployee.java
+++ src/main/java/com/hb14/entity_life_cycle/somemethods/RunnerFetchEmployee.java
@@ -5,10 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 public class RunnerFetchEmployee {
     public static void main(String[] args) {
         Configuration con = new Configuration().
